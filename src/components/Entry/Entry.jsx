b50feaf57b40bf7e539049a39f8ec0e33111b5f6
Maisha Mir <maisha.mir2014@gmail.com>
2025-11-30 23:20:00 -0800

    termComponent styled

--- src/components/Entry/Entry.jsx
+++ src/components/Entry/Entry.jsx
@@ -30,6 +30,7 @@ function EntryTerm({ meaning }) {
                 <p>{partOfSpeech}</p>
                 <div className="divider"></div>
             </div>
+            <p className="label">Meaning</p>
             <ul className="entry__meanings">
                 {definitions.map((def, i) => (
                     <li key={i}>
@@ -41,7 +42,7 @@ function EntryTerm({ meaning }) {
                 ))}
             </ul>
             {synonyms && synonyms.length > 0 && (
-                <div>
+                <div className="synonym__list">
                     <p className="label">Synonyms</p>
                     <ul className="synonyms">
                         {synonyms.map((syn, i) => (
@@ -72,23 +73,31 @@ function Entry({ entry, entryNotFound }) {
 
     return (
         <section className="entry">
-            <h1 className="entry__term">{entry.word}</h1>
-            <p className="body-m">{entry.phonetic}</p>
-            <AudioPlayer url={audioUrl} />
-            <div>
+            <div className="entry__word">
+                <div className="entry__term-phone">
+                    <h1 className="entry__term">{entry.word}</h1>
+                    <p className="body-m entry__phonetic">{entry.phonetic}</p>
+                </div>
+                <AudioPlayer url={audioUrl} />
+            </div>
+            <div className="entry__definitions">
                 {entry.meanings.map((meaning, i) => (
                     <EntryTerm meaning={meaning} key={i} />
                 ))}
             </div>
             <hr />
-            <small>Source</small>
-            <a
-                href={entry.sourceUrls[0]}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                {entry.sourceUrls[0]} <img src={newWindow} alt="" />
-            </a>
+
+            <div className="entry__source">
+                <small className="body-s">Source</small>
+                <a
+                    href={entry.sourceUrls[0]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="body-s"
+                >
+                    <p className="source__link">{entry.sourceUrls[0]} <img src={newWindow} alt="" className="newWindowIcon"/></p>
+                </a>
+            </div>
         </section>
     );
 }
