72d56ecb46482f2c0812ebe8f9cc77a14ee1c03b
Maisha Mir <maisha.mir2014@gmail.com>
2025-12-01 00:29:48 -0800

    header component styled; font switcher style WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.scss";
-import logo from "./assets/images/logo.svg";
 import Entry from "./components/Entry/Entry";
 import SearchBar from "./components/SearchBar/SearchBar";
+import Header from "./components/Header/Header";
 
 const BASE = "https://api.dictionaryapi.dev/api/v2/entries/en";
 
@@ -13,12 +13,16 @@ function App() {
 
   const [entry, setEntry] = useState("keyboard");
   const [entryData, setEntryData] = useState("");
-  // const [isError, setIsError] = useState(false);
   const [errData, setErrData] = useState("")
 
   function handleSearch(entryInput) {
     setEntry(entryInput);
     console.log(entryInput);
+  }
+
+  function handleFontClass(fontClassInput) {
+    setFontClass(fontClassInput);
+    console.log(fontClassInput)
   }
 
   useEffect(() => {
@@ -39,27 +43,8 @@ function App() {
 
   return (
     <main className={`app ${fontClass}`} data-theme={isDark ? "dark" : "light"}>
-      <header>
-        <img src={logo} alt="" />
 
-        <form action="">
-          <select name="fontStyle" id="fontStyle" className="fontStyle" onChange={(e) => setFontClass(e.target.value)}>
-            <option
-              value="sans-serif"
-
-            >
-              Sans Serif
-            </option>
-            <option value="serif" >
-              Serif
-            </option>
-            <option value="mono" >
-              Mono
-            </option>
-          </select>
-        </form>
-
-      </header>
+      <Header onFontSwitch={handleFontClass} />
 
       <SearchBar onSearch={handleSearch} />
       <Entry entry={entryData} entryNotFound={errData} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import logo from "../../assets/images/logo.svg";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import "./Header.scss";
+
+function Header({ onFontSwitch }) {
+
+    const [fontClass, setFontClass] = useState("sans-serif");
+
+    return (
+        <header className='header'>
+            <img src={logo} alt="" className='header__logo' />
+
+            <div className="header__toggles">
+                <form action="">
+                    <select name="fontStyle" id="fontStyle" className="fontStyle" onChange={(e) => onFontSwitch(e.target.value)}>
+                        <option
+                            value="sans-serif"
+
+                        >
+                            Sans Serif
+                        </option>
+                        <option value="serif" >
+                            Serif
+                        </option>
+                        <option value="mono" >
+                            Mono
+                        </option>
+                    </select>
+                </form>
+                <div className="header__divider"></div>
+                <ThemeToggle />
+            </div>
+        </header>
+    )
+}
+
+export default Header
